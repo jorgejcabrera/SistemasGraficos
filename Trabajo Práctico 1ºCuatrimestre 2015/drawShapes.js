edff825552ec18fbc0c037048ab89870b83656d8
Fernando Cancellara <fdcancellara@gmail.com>
2015-04-21 16:11:36 -0300

    Boceto de funciones para familiarizarse
    Subo este primer boceto como para irme familiarizando con las funciones
    de webgl

---
+++ Trabajo Práctico 1ºCuatrimestre 2015/drawShapes.js
@@ -0,0 +1,78 @@
+function drawTriangle (where) {	
+	mat4.identity(mvMatrix);
+
+	mat4.translate(mvMatrix, where);
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+}
+
+function drawSquare (where) {
+	mat4.identity(mvMatrix);
+
+	mat4.translate(mvMatrix, where);
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+}
+
+function drawCube (where) {
+	mat4.identity(mvMatrix);
+	
+	mat4.translate(mvMatrix, where);
+	mat4.rotate(mvMatrix, degToRad(ticker), [0, 1, 0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+	setMatrixUniforms();
+	gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+}
+
+function drawCube2 (where) {
+	mat4.identity(mvMatrix);	
+
+	//primera cara	
+	mat4.translate(mvMatrix, where);
+	mat4.rotate(mvMatrix,degToRad(-45),[0, 1, 0]);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cube2VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cube2VertexPositionBuffer.numItems);	
+	//segunda cara
+	mat4.translate(mvMatrix, [1, 0, -1]);	
+	mat4.rotate(mvMatrix,degToRad(90),[0, 1, 0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cube2VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cube2VertexPositionBuffer.numItems);	
+	//tercera cara	
+	mat4.translate(mvMatrix, [-1, 0, -1]);
+	mat4.rotate(mvMatrix,degToRad(90),[0, 1, 0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cube2VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cube2VertexPositionBuffer.numItems);
+	//cuarta cara
+	mat4.translate(mvMatrix, [-1, 0, 1]);
+	mat4.rotate(mvMatrix,degToRad(90),[0, 1, 0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cube2VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cube2VertexPositionBuffer.numItems);
+	//tapa arriba
+	mat4.translate(mvMatrix, [1, 1, 0]);
+	mat4.rotate(mvMatrix,degToRad(90),[1, 0, 0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cube2VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cube2VertexPositionBuffer.numItems);
+	//tapa abajo
+	mat4.translate(mvMatrix, [0, 0, 2]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cube2VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cube2VertexPositionBuffer.numItems);
+}
---
+++ Trabajo Práctico 1ºCuatrimestre 2015/shapes.js
@@ -0,0 +1,73 @@
+function triangle () {
+	triangleVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	var vertices = [
+		 0.0,  1.0,  0.0,
+		-1.0, -1.0,  0.0,
+		 1.0, -1.0,  0.0
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	triangleVertexPositionBuffer.itemSize = 3;
+	triangleVertexPositionBuffer.numItems = 3;
+}
+
+function square () {
+	squareVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+	var vertices = [
+		-1.0,	1.0,	0.0,
+		1.0,	1.0,	0.0,
+		-1.0,	-1.0,	0.0,
+		1.0,	-1.0,	0.0
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	squareVertexPositionBuffer.itemSize = 3;
+	squareVertexPositionBuffer.numItems = 4;
+}
+
+function cube () {
+	cubeVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+		var vertices = [
+		-1.0,	1.0,	1.0,
+		1.0,	1.0,	1.0,
+		-1.0,	-1.0,	1.0,
+		1.0,	-1.0,	1.0,
+		-1.0,	1.0,	-1.0,
+		1.0,	1.0,	-1.0,
+		-1.0,	-1.0,	-1.0,
+		1.0,	-1.0,	-1.0
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	cubeVertexPositionBuffer.itemSize = 3;
+	cubeVertexPositionBuffer.numItems = 8;
+	
+	//Y luego para usar el vertex index:
+	cubeVertexIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+	var cubeVertexIndices = [
+		0, 1, 2,      1, 2, 3,    // Front face
+		4, 5, 6,      5, 6, 7,    // Back face
+		0, 1, 4,     1, 4, 5,  // Top face
+		2, 3, 6,   3, 6, 7, // Bottom face
+		1, 3, 5,   3, 5, 7, // Right face
+		0, 2, 4,   2, 4, 6  // Left face
+	]
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+	cubeVertexIndexBuffer.itemSize = 1;
+	cubeVertexIndexBuffer.numItems = 36;
+}
+
+function cube2 () {
+	cube2VertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube2VertexPositionBuffer);
+		var vertices = [
+		-1.0,	1.0,	0.0,
+		1.0,	1.0,	0.0,
+		-1.0,	-1.0,	0.0,
+		1.0,	-1.0,	0.0
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	cube2VertexPositionBuffer.itemSize = 3;
+	cube2VertexPositionBuffer.numItems = 4;
+}
