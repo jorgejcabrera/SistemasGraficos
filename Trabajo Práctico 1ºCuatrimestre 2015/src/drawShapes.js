ba0fcacd45fcc56da677812423c55e2d05cffeeb
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-23 21:03:19 -0300

    refactorizo había muco código duplicado en la librería encargada de dibujar

--- Trabajo Práctico 1ºCuatrimestre 2015/src/drawShapes.js
+++ Trabajo Práctico 1ºCuatrimestre 2015/src/drawShapes.js
@@ -1,48 +1,32 @@
-function drawTriangle (where) {	
-	mat4.identity(mvMatrix);
-
-	mat4.translate(mvMatrix, where);
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	setMatrixUniforms();
+function drawTriangle (where,scalator) {	
+	drawShaper3D(where,scalator,triangleVertexPositionBuffer,false)
 	gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
 }
 
 function drawSquare (where,scalator) {
-	mat4.identity(mvMatrix);
-	
-
-	mat4.translate(mvMatrix, where);
-	mat4.rotate(mvMatrix, degToRad(ticker), [1, 1, 1]);
-	mat4.scale(mvMatrix,scalator);
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	setMatrixUniforms();
+	drawShaper3D(where,scalator,squareVertexPositionBuffer,true)
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
 }
 
 function drawCube (where,scalator) {
-	mat4.identity(mvMatrix);
-	
-	//notar que el orden es importante, para OpenGL y WebGL el orden es TRASLADAR, ROTAR luego PROPORCIONAR
-	mat4.translate(mvMatrix, where);	
-	//mat4.rotate(mvMatrix, degToRad(ticker), [0, 1, 0]);
-	mat4.scale(mvMatrix,scalator);
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-	setMatrixUniforms();
+	drawShaper3D(where,scalator,cubeVertexPositionBuffer,true)
 	gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
 function drawPyramid(where,scalator){
-	mat4.identity(mvMatrix);
-	
-	mat4.translate(mvMatrix, where);
-	//mat4.rotate(mvMatrix, degToRad(ticker), [1, 1, 1]);
-	mat4.scale(mvMatrix,scalator);
-	gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, pyramidVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	setMatrixUniforms();
+	drawShaper3D(where,scalator,pyramidVertexPositionBuffer,false)
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, pyramidVertexPositionBuffer.numItems);
+}
+
+/*La funcion se encarga de dibujar el objeto segun el buffer que se le pase, la posicion y la escala*/
+function drawShaper3D(where,scalator,buffer,animation){
+	mat4.identity(mvMatrix);
+	mat4.translate(mvMatrix, where);
+	if (animation == true){
+		mat4.rotate(mvMatrix, degToRad(ticker), [1, 1, 1]);
+	}
+	mat4.scale(mvMatrix,scalator);
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, buffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
 }
