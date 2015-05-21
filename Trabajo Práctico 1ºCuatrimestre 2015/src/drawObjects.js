f0944a52d7641333bbe293ba8d78c96633510041
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-05-21 20:52:24 -0300

    se arregla las posiciones donde se dibuja la grua acorde a la posicion de nuestra camara

--- Trabajo Práctico 1ºCuatrimestre 2015/src/drawObjects.js
+++ Trabajo Práctico 1ºCuatrimestre 2015/src/drawObjects.js
@@ -1,22 +1,13 @@
 
 //la posY indica la posicion de donde se empieza a dibujar el muro
 function drawWallCrane(posX,posY,posZ){
-	var height = 0.3;
-	drawCube([posX, posY, posZ],[0.1, height, 0.1]);
-	drawCube([posX, posY+height*2, posZ],[0.1, height, 0.1]);
-	drawCube([posX, posY+height*4, posZ],[0.1, height, 0.1]);
-	drawCube([posX, posY+height*6, posZ],[0.1, height, 0.1]);
-	drawCube([posX, posY+height*8, posZ],[0.1, height, 0.1]);
-	
-	drawCube([0, posY * 22, posZ],[height*3, 0.1, 0.1]);
-	//drawCube([0, posY * 22, posZ],[height*3, 0.1, 0.1]);
-
-	drawCube([-posX, posY, posZ],[0.1, height, 0.1]);
-	drawCube([-posX, posY+height*2, posZ],[0.1, height, 0.1]);
-	drawCube([-posX, posY+height*4, posZ],[0.1, height, 0.1]);
-	drawCube([-posX, posY+height*6, posZ],[0.1, height, 0.1]);
-	drawCube([-posX, posY+height*8, posZ],[0.1, height, 0.1]);
-
-	drawCube([0, -0.1, posZ],[height*3, 0.1, 0.1]);
+	var height = 4.0;
+	var width = 0.1;
+	var longitude = 0.2;
+	var scale = [longitude,width,height];
+	drawCube([posX,posY,posZ],scale);
+	drawCube([posX, posY+height, posZ],scale);
+	drawCube([posX, posY + longitude * 10, posZ - longitude * 4],[longitude,(height-longitude)/2,width]);
+	drawCube([posX, posY + longitude * 10, height + posZ - width],[longitude,(height-longitude)/2,width]);
 }
 
