ef536b5ddbf7b323a7070803122d50709043be7a
Fernando Cancellara <fdcancellara@gmail.com>
2015-05-31 20:21:30 -0300

    Mejorado rendimiento del cilindro
    Igual le falta

--- Trabajo Práctico 1ºCuatrimestre 2015/src/drawShapes.js
+++ Trabajo Práctico 1ºCuatrimestre 2015/src/drawShapes.js
@@ -23,7 +23,7 @@ function drawPyramid(where,scalator,degreesToRotate){
 }
 
 function drawCylinder(where, scalator,degreesToRotate){
-	cylinder(7200,1);
+	cylinder(8,1);
 	drawShaper3D(where,scalator,cylinderVertexPositionBuffer,true,nullTexture,cylinderVertexTextureCoordBuffer,degreesToRotate);
 	gl.drawElements(gl.TRIANGLES, cylinderVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
