4a00ad1a99d3a610901359f86d65cdd74b878804
Fernando Cancellara <fdcancellara@gmail.com>
2015-06-04 17:57:05 -0300

    Cambios y overloads de funciones para deshardcodear minimas cosas

--- Trabajo Práctico 1ºCuatrimestre 2015/src/drawObjects.js
+++ Trabajo Práctico 1ºCuatrimestre 2015/src/drawObjects.js
@@ -7,15 +7,15 @@ function drawWallCrane(posX,posY,posZ){
 	var longitudeHypotenuse = calculateHypotenuse(height - posZ + longitude * 1.50 - longitude * 7, (height-longitude)/2 + width);
 	var scale = [longitude,width,height];
 	//columna 1
-	drawCube([posX,posY,posZ],scale,0);
+	drawCube([posX,posY,posZ],scale);
 	//columna 2
-	drawCube([posX, posY + height, posZ],scale,0);
+	drawCube([posX, posY + height, posZ],scale);
 	//barra lateral baja
-	drawCube([posX, posY + longitude * 10, posZ - longitude * 1.50],[longitude,(height-longitude)/2,width],0);
+	drawCube([posX, posY + longitude * 10, posZ - longitude * 1.50],[longitude,(height-longitude)/2,width]);
 	//barra lateral alta
-	drawCube([posX, posY + longitude * 10, height + posZ - width],[longitude,(height-longitude)/2,width],0);
+	drawCube([posX, posY + longitude * 10, height + posZ - width],[longitude,(height-longitude)/2,width]);
 	//barra diagonal			esta todo muy hardcodeado hay que optimizar
-	drawCube([posX, posY+longitude*10, height-longitude*6],[longitude*0.5,longitudeHypotenuse,width],Math.PI/4);
+	drawCubeOverload([posX, posY+longitude*10, height-longitude*6],[longitude*0.5,longitudeHypotenuse,width],45,[1,0,0]);
 }
 
 //PRE: POSX TIENE QUE SER DISTINTO DE CERO
@@ -30,18 +30,18 @@ function drawCrame(posX,posY,posZ){
     drawWallCrane( posX+distanceBeetwenWalls,posY,posZ);
 
     //BARRAS HORIZONTALES
-    drawCube([ posX+distanceBeetwenWalls*0.5, posY+longitude*4, height+posZ-width ],[distanceBeetwenWalls-height/2-longitude , width*2,width],0);
-    drawCube([ posX+distanceBeetwenWalls*0.5, posY+height-longitude*4 , height+posZ-width ],[distanceBeetwenWalls-height/2-longitude ,width*2,width],0);
+    drawCube([ posX+distanceBeetwenWalls*0.5, posY+longitude*4, height+posZ-width ],[distanceBeetwenWalls-height/2-longitude , width*2,width]);
+    drawCube([ posX+distanceBeetwenWalls*0.5, posY+height-longitude*4 , height+posZ-width ],[distanceBeetwenWalls-height/2-longitude ,width*2,width]);
 
     //SOPORTE
-    drawCube([posX+distanceBeetwenWalls*0.5+(posX+distanceBeetwenWalls*0.5)/5 , posY - longitude * 6 , height + posZ - width],[longitude,distanceBeetwenWalls * 1.5,width*1.5],0);
-   	drawCube([posX+distanceBeetwenWalls*0.5-(posX+distanceBeetwenWalls*0.5)/5 , posY - longitude * 6 , height + posZ - width],[longitude,distanceBeetwenWalls * 1.5,width*1.5],0);
+    drawCube([posX+distanceBeetwenWalls*0.5+(posX+distanceBeetwenWalls*0.5)/5 , posY - longitude * 6 , height + posZ - width],[longitude,distanceBeetwenWalls * 1.5,width*1.5]);
+   	drawCube([posX+distanceBeetwenWalls*0.5-(posX+distanceBeetwenWalls*0.5)/5 , posY - longitude * 6 , height + posZ - width],[longitude,distanceBeetwenWalls * 1.5,width*1.5]);
 
     //UNION ENTRE AMBOS SOPORTES
-    drawCube([posX+distanceBeetwenWalls*0.5, posY-longitude*6-distanceBeetwenWalls*1.5- width, height + posZ - width ],[ (posX + distanceBeetwenWalls * 0.5) * 1/5 + width * 2,  width, width * 1.5],0);
+    drawCube([posX+distanceBeetwenWalls*0.5, posY-longitude*6-distanceBeetwenWalls*1.5- width, height + posZ - width ],[ (posX + distanceBeetwenWalls * 0.5) * 1/5 + width * 2,  width, width * 1.5]);
 
     //CONTRAPESO
-    drawCube([posX+distanceBeetwenWalls*0.5, posY-longitude*6+distanceBeetwenWalls*1.5, height + posZ - width ],[ (posX + distanceBeetwenWalls * 0.5) * 1/5 + width * 3, distanceBeetwenWalls * 0.15, distanceBeetwenWalls * 0.15],0);
+    drawCube([posX+distanceBeetwenWalls*0.5, posY-longitude*6+distanceBeetwenWalls*1.5, height + posZ - width ],[ (posX + distanceBeetwenWalls * 0.5) * 1/5 + width * 3, distanceBeetwenWalls * 0.15, distanceBeetwenWalls * 0.15]);
 
     //CABINA
     drawCabin(posX, posY, height, posZ );
@@ -58,47 +58,47 @@ function drawCabin(posX,posY,posZ,height){
 	var posYRearWall = posY-longitude*6-distanceBeetwenWalls*1.5-width+longitudeRearWall*0.75*6;
 
 	//PARED TRASERA
-	drawCube([posXRearWall, posYRearWall, posZ],[longitudeRearWall,width, height-width],0);
+	drawCube([posXRearWall, posYRearWall, posZ],[longitudeRearWall,width, height-width]);
 
 	//PISO
-	drawCube([posXRearWall, posYRearWall-longitudeRearWall*0.75-width, posZ-height+width*2],[longitudeRearWall,longitudeRearWall*0.75, width],0);
-	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width,posZ-height+width*7],[width,longitudeRearWall*0.75, width*4],0);
-	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width,posZ-height+width*7],[width,longitudeRearWall*0.75, width*4],0);
-	drawCube([posXRearWall,posYRearWall-longitudeRearWall*0.75*2,posZ-height+width*7],[longitudeRearWall-width*2,width, width*4],0);
+	drawCube([posXRearWall, posYRearWall-longitudeRearWall*0.75-width, posZ-height+width*2],[longitudeRearWall,longitudeRearWall*0.75, width]);
+	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width,posZ-height+width*7],[width,longitudeRearWall*0.75, width*4]);
+	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width,posZ-height+width*7],[width,longitudeRearWall*0.75, width*4]);
+	drawCube([posXRearWall,posYRearWall-longitudeRearWall*0.75*2,posZ-height+width*7],[longitudeRearWall-width*2,width, width*4]);
 
 	//TECHO
-	drawCube([posXRearWall, posYRearWall-longitudeRearWall*0.75-width, posZ+height-width*2],[longitudeRearWall,longitudeRearWall*0.75, width],0);
-	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width, posZ+height-width*7],[width,longitudeRearWall*0.75, width*4],0);
-	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width, posZ+height-width*7],[width,longitudeRearWall*0.75, width*4],0);
-	drawCube([posXRearWall, posYRearWall-longitudeRearWall*0.75*2, posZ+height-width*7],[longitudeRearWall-width*2,width, width*4],0);
+	drawCube([posXRearWall, posYRearWall-longitudeRearWall*0.75-width, posZ+height-width*2],[longitudeRearWall,longitudeRearWall*0.75, width]);
+	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width, posZ+height-width*7],[width,longitudeRearWall*0.75, width*4]);
+	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width, posZ+height-width*7],[width,longitudeRearWall*0.75, width*4]);
+	drawCube([posXRearWall, posYRearWall-longitudeRearWall*0.75*2, posZ+height-width*7],[longitudeRearWall-width*2,width, width*4]);
 
 	//LATERALES IZQUIERDOS
-	drawCube([posXRearWall-longitudeRearWall+width,posYRearWall-width*4, posZ],[width,longitudeRearWall*0.75*0.25, height-11*width],0);
-	drawCube([posXRearWall-longitudeRearWall+width,posYRearWall-longitudeRearWall*0.75*2+longitudeRearWall*0.75*0.25-width,posZ],[width,longitudeRearWall*0.75*0.25, height-11*width],0);
+	drawCube([posXRearWall-longitudeRearWall+width,posYRearWall-width*4, posZ],[width,longitudeRearWall*0.75*0.25, height-11*width]);
+	drawCube([posXRearWall-longitudeRearWall+width,posYRearWall-longitudeRearWall*0.75*2+longitudeRearWall*0.75*0.25-width,posZ],[width,longitudeRearWall*0.75*0.25, height-11*width]);
 
 	//LATERALES DERECHOS	
-	drawCube([posXRearWall+longitudeRearWall-width,posYRearWall-width*4,posZ],[width,longitudeRearWall*0.75*0.25, height-width-2*(width*5)],0);
-	drawCube([posXRearWall+longitudeRearWall-width,posYRearWall-longitudeRearWall*0.75*2+longitudeRearWall*0.75*0.25-width,posZ],[width,longitudeRearWall*0.75*0.25, height-11*width],0);
+	drawCube([posXRearWall+longitudeRearWall-width,posYRearWall-width*4,posZ],[width,longitudeRearWall*0.75*0.25, height-width-2*(width*5)]);
+	drawCube([posXRearWall+longitudeRearWall-width,posYRearWall-longitudeRearWall*0.75*2+longitudeRearWall*0.75*0.25-width,posZ],[width,longitudeRearWall*0.75*0.25, height-11*width]);
 
 	//PARTE FRONTAL
-	drawCube([posXRearWall+longitudeRearWall-width*2-longitudeRearWall*0.75*0.25,posYRearWall-longitudeRearWall*0.75*2,posZ],[longitudeRearWall*0.75*0.25,width, height-width-2*(width*5)],0);
-	drawCube([posXRearWall-longitudeRearWall+width*2+longitudeRearWall*0.75*0.25,posYRearWall-longitudeRearWall*0.75*2,posZ],[longitudeRearWall*0.75*0.25,width, height-width-2*(width*5)],0);
+	drawCube([posXRearWall+longitudeRearWall-width*2-longitudeRearWall*0.75*0.25,posYRearWall-longitudeRearWall*0.75*2,posZ],[longitudeRearWall*0.75*0.25,width, height-width-2*(width*5)]);
+	drawCube([posXRearWall-longitudeRearWall+width*2+longitudeRearWall*0.75*0.25,posYRearWall-longitudeRearWall*0.75*2,posZ],[longitudeRearWall*0.75*0.25,width, height-width-2*(width*5)]);
 
 	//PINZAS IZQUIERDAS
-	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width-width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95],0);
-	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width+width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95],0);
+	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width-width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95]);
+	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width+width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95]);
 
 	//PINZAS DERECHAS
 	/*como la longitud de las pinzas es de la altura de la grua: posZ=4 es 4*0.95 que da 3.8,los container tendran que tener de altura 0.2. CUANDO LEVANTAMOS UN CONTAINER LO QUE
 	VAMOS A TENER QUE VARIAR ES EL PARAMETRO CONRRESPONDIENTE A LA ESCALA NO LA POSICION EN Z DE LAS PINZAS*/
-	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width-width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95],0);
-	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width+width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95],0);
+	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width-width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95]);
+	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width+width*6,posZ*0.5-(height-width)],[width*0.25,width*0.25,posZ*0.95]);
 	
 	//AGARRE IZQUIERDO
-	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width,posZ*0.5-(height-width)-posZ*0.95],[width,width*10,width*.25],0);
+	drawCube([posXRearWall-longitudeRearWall+width, posYRearWall-longitudeRearWall*0.75-width,posZ*0.5-(height-width)-posZ*0.95],[width,width*10,width*.25]);
 	
 	//AGARRE DERECHO
-	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width,posZ*0.5-(height-width)-posZ*0.95],[width,width*10,width*.25],0);
+	drawCube([posXRearWall+longitudeRearWall-width, posYRearWall-longitudeRearWall*0.75-width,posZ*0.5-(height-width)-posZ*0.95],[width,width*10,width*.25]);
 
 }
 
--- Trabajo Práctico 1ºCuatrimestre 2015/src/drawShapes.js
+++ Trabajo Práctico 1ºCuatrimestre 2015/src/drawShapes.js
@@ -1,47 +1,73 @@
-function drawTriangle (where,scalator,degreesToRotate) {
+function drawTriangle (where,scalator) {
 	//por ahora hardcodeada la textura
-	drawShaper3D(where,scalator,triangleVertexPositionBuffer,false,nullTexture,degreesToRotate);
+	drawShaper3D(where,scalator,triangleVertexPositionBuffer,false,nullTexture);
 	gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
 }
 
-function drawSquare (where,scalator,degreesToRotate) {
+function drawSquare (where,scalator) {
 	//por ahora hardcodeada la textura
-	drawShaper3D(where,scalator,squareVertexPositionBuffer,true,nullTexture,degreesToRotate);
+	drawShaper3D(where,scalator,squareVertexPositionBuffer,true,nullTexture);
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
 }
 
-function drawCube (where,scalator,degreesToRotate) {
+function drawCube (where,scalator) {
 	//por ahora hardcodeada la textura
-	drawShaper3D(where,scalator,cubeVertexPositionBuffer,false,craneTexture,cubeVertexTextureCoordBuffer,degreesToRotate);
+	drawShaper3D(where,scalator,cubeVertexPositionBuffer,false,craneTexture,cubeVertexTextureCoordBuffer);
+	gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+}
+//Overload para que le pase los grados y el eje de rotacion
+function drawCubeOverload (where,scalator,degreesToRotate,axisToRotate) {
+	//por ahora hardcodeada la textura
+	drawShaper3DOverload(where,scalator,cubeVertexPositionBuffer,false,craneTexture,cubeVertexTextureCoordBuffer,degreesToRotate,axisToRotate);
 	gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
-function drawPyramid(where,scalator,degreesToRotate){
+function drawPyramid(where,scalator){
 	//por ahora hardcodeada la textura
-	drawShaper3D(where,scalator,pyramidVertexPositionBuffer,false,nullTexture,degreesToRotate);
+	drawShaper3D(where,scalator,pyramidVertexPositionBuffer,false,nullTexture);
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, pyramidVertexPositionBuffer.numItems);
 }
 
-function drawCylinder(where, scalator,degreesToRotate){
+function drawCylinder(where, scalator,degreesToRotate,axisToRotate){
 	cylinder(360,1);
-	drawShaper3D(where,scalator,cylinderVertexPositionBuffer,false,wheelTexture,cylinderVertexTextureCoordBuffer,degreesToRotate);
+	drawShaper3DOverload(where,scalator,cylinderVertexPositionBuffer,false,wheelTexture,cylinderVertexTextureCoordBuffer,degreesToRotate,axisToRotate);
 	gl.drawElements(gl.TRIANGLES, cylinderVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
-function drawSky(where, scalator,degreesToRotate){
-		sky = new TexturedSphere(32, 32);
-        sky.initBuffers();
-		drawShaper3D(where,scalator,sky.webgl_position_buffer,false,skyTexture,sky.webgl_texture_coord_buffer,degreesToRotate);
-		gl.drawElements(gl.TRIANGLES, sky.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
+function drawSky(where, scalator,degreesToRotate,axisToRotate){
+	sky = new TexturedSphere(32, 32);
+	sky.initBuffers();
+	drawShaper3DOverload(where,scalator,sky.webgl_position_buffer,false,skyTexture,sky.webgl_texture_coord_buffer,degreesToRotate,axisToRotate);
+	gl.drawElements(gl.TRIANGLES, sky.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
 /*La funcion se encarga de dibujar el objeto segun el buffer que se le pase, la posicion y la escala*/
-//TODO: CAMBIAR cubeVertexTextureCoordBuffer que no va en el shaper 3d, pasar el vertexTextureCoorBuffer por parametro
-function drawShaper3D(where,scalator,vertexBuffer,animation,texture,vertexTextureBuffer,degreesToRotate){
+function drawShaper3D(where,scalator,vertexBuffer,animation,texture,vertexTextureBuffer){
 	mat4.lookAt(vMatrix,cameraPosition,target,[0.0,0.0,1.0]);
 	mat4.identity(mMatrix);
 	mat4.translate(mMatrix,mMatrix, where);
-	mat4.rotate(mMatrix, mMatrix, degToRad(degreesToRotate), [1, 0, 0]);
+
+	if (animation == true){
+		mat4.rotate(mMatrix, mMatrix, degToRad(ticker), [1, 1, 1]);
+	}
+	mat4.scale(mMatrix, mMatrix,scalator);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexTextureBuffer);
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, vertexTextureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.uniform1i(shaderProgram.samplerUniform, 0);	
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+}
+//Overload que pide rotación y eje de rotación
+function drawShaper3DOverload(where,scalator,vertexBuffer,animation,texture,vertexTextureBuffer,degreesToRotate,axisToRotate){
+	mat4.lookAt(vMatrix,cameraPosition,target,[0.0,0.0,1.0]);
+	mat4.identity(mMatrix);
+	mat4.translate(mMatrix,mMatrix, where);
+	mat4.rotate(mMatrix, mMatrix, degToRad(degreesToRotate), axisToRotate);	//axisToRotate = 1 0 0 90 grados
 
 	if (animation == true){
 		mat4.rotate(mMatrix, mMatrix, degToRad(ticker), [1, 1, 1]);
